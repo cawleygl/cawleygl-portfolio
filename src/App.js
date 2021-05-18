4bf7fad3855c4c437ed943022ecfc423cd5f9930
cawleygl <grantlcawley@gmail.com>
2021-05-17 21:03:49 -0400

    Created initial page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Portfolio from "./pages/Portfolio"
 import './App.css';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <Route exact path="/" component={About} />
+            <Route export path="/contact" component={Contact} />
+            <Route export path="/portfolio" component={Portfolio} />
+</Router>
+
   );
 }
 
---
+++ src/pages/About/index.js
@@ -0,0 +1,18 @@
+// import React, { useEffect, useState } from 'react'
+// import { useAppContext } from '../../utils/AppContext'
+import "./style.css"
+
+const AboutPage = () => {
+    return (
+        <div className="container mt-sm-5 mt-4">
+            <div class="row p-2">
+                <div class="col-sm-10 ml-sm-4 border-bottom border-dark">
+                    <h1 class="font-weight-light">About Me</h1>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutPage
