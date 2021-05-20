478675cee5e24b17108942ee6e237a2b44bc37d0
cawleygl <grantlcawley@gmail.com>
2021-05-19 22:20:33 -0400

    Updated Navbar style

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,8 +6,8 @@ const navbar = () => {
         <>
         <Navbar collapseOnSelect sticky='top' expand='sm' bg='dark' variant='dark'>
             <Container>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Brand>Grant Cawley</Navbar.Brand>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                 <Navbar.Collapse className='justify-content-end' id='responsive-navbar-nav'>
                     <Nav>
                         <Nav.Link href='/'>About</Nav.Link>
