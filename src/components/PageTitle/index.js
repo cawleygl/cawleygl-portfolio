57553c2f34b5eb62340d39c572562f67f0279a4a
cawleygl <grantlcawley@gmail.com>
2021-06-01 21:57:19 -0400

    Updated title style

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Jumbotron, Container } from 'react-bootstrap';
 
+import "./style.css"
+
 const pageTitle = (props) => {
     return (
-        <Jumbotron>
+        <Jumbotron class="jumbotron">
             <Container>
-                <h1>{props.title}</h1>
+                <h1 class="title">{props.title}</h1>
                 <p>{props.subtitle}</p>
             </Container>
         </Jumbotron>
