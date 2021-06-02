585f809f3f464670fde94b7c6a0befbf5cd9d8b5
cawleygl <grantlcawley@gmail.com>
2021-06-01 21:30:52 -0400

    Changed page to utilize props and components

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,81 +1,28 @@
 // import React, { useEffect, useState } from 'react'
 // import { useAppContext } from '../../utils/AppContext'
+import PageTitle from '../../components/PageTitle'
+import ProjectCard from '../../components/ProjectCard'
+
+//Project screenshot images from Assets folder
+import Burger from '../../Assets/BurgerEater.png'
+import Recipe from '../../Assets/RecipeGenerator.png'
+import Weather from '../../Assets/WeatherDashboard.png'
+import Planner from '../../Assets/WorkdayPlanner.png'
+
 import "./style.css"
 
 const PortfolioPage = () => {
     return (
         <div className="container mt-sm-5 mt-4">
-            <div class="row p-2">
-                <div class="col-sm-10 ml-sm-4 border-bottom border-dark">
-                    <h1 class="font-weight-light">Portfolio</h1>
-                </div>
-                <div class="col-md-12">
-                    <p>A sample of the coding projects and assignments I've completed, with links to deployed projects and
-                    GitHub repositories.</p>
-                </div>
+            <PageTitle title="Portfolio" subtitle="A sample of the coding projects and assignments I've completed, with links to deployed projects and GitHub repositories"></PageTitle>
 
-            </div>
-            <div class="container-fluid px-md-5">
-                <div class="row mb-md-3">
-                    <div class="col-md-4">
-                        <div class="card m-2">
-                            <img src="Assets/RecipeGenerator.png" class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Nutritious Recipe Generator</h5>
-                                <p class="card-text">A web application that allows users to quickly display recipes from a
-                            recipe API in different ways, to inspire creativity in the kitchen.</p>
-                                <a href="https://egivilza.github.io/NutritiousRecipeGenerator/" class="card-link"
-                                    target="_blank">Application</a>
-                                <a href="https://github.com/EgiVilza/NutritiousRecipeGenerator" class="card-link"
-                                    target="_blank">Repository</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card m-2">
-                            <img src="Assets/BurgerEater.png" class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Burger Eater</h5>
-                                <p class="card-text">A simple application that allows the user to submit names of burgers, then
-                                "eat" them by clicking a button that populates next to it's name.
-                        </p>
-                                <a href="https://floating-ocean-83726.herokuapp.com/" class="card-link" target="_blank">Application</a>
-                                <a href="https://github.com/cawleygl/burger" class="card-link" target="_blank">Repository</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card m-2">
-                            <img src="Assets/WorkdayPlanner.png" class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Work Day Scheduler</h5>
-                                <p class="card-text">An application that allows the user to schedule out events in a work day,
-                                save them for later reference, and see which events have passed and which are still to come.
-                        </p>
-                                <a href="https://cawleygl.github.io/workday-planner/" class="card-link" target="_blank">Application</a>
-                                <a href="https://github.com/cawleygl/workday-planner" class="card-link" target="_blank">Repository</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <ProjectCard asset={Burger} title="Burger Eater" text="A simple application that allows the user to submit names of burgers, then 'eat' them by clicking a button that populates next to it's name." appLink="https://floating-ocean-83726.herokuapp.com/" repoLink="https://github.com/cawleygl/burger"></ProjectCard>
 
-                <div class="row mb-md-3">
-                    <div class="col-md-4">
-                        <div class="card m-2">
-                            <img src="Assets/WeatherDashboard.png" class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Weather Dashboard</h5>
-                                <p class="card-text">A weather dashboard application that shows current weather
-                                conditions in
-                            any city, as well as a 5-day forecast.</p>
-                                <a href="https://cawleygl.github.io/weather-dashboard/" class="card-link"
-                                    target="_blank">Application</a>
-                                <a href="https://github.com/cawleygl/weather-dashboard" class="card-link" target="_blank">Repository</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProjectCard asset={Recipe} title="Nutritious Recipe Generator" text="A web application that allows users to quickly display recipes from a recipe API in different ways, to inspire creativity in the kitchen." appLink="https://egivilza.github.io/NutritiousRecipeGenerator/" repoLink="https://github.com/EgiVilza/NutritiousRecipeGenerator"></ProjectCard>
+
+            <ProjectCard asset={Weather} title="Weather Dashboard" text="A weather dashboard application that shows current weather conditions in any city, as well as a 5-day forecast" appLink="https://cawleygl.github.io/weather-dashboard/" repoLink="https://github.com/cawleygl/weather-dashboard"></ProjectCard>
+
+            <ProjectCard asset={Planner} title="Work Day Scheduler" text="An application that allows the user to schedule out events in a work day, save them for later reference, and see which events have passed and which are still to come." appLink="https://cawleygl.github.io/workday-planner/" repoLink="https://github.com/cawleygl/workday-planner"></ProjectCard>
 
         </div>
     )
