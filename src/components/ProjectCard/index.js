b7e886dcc11de4a12f44f931184192f6f238f90e
cawleygl <grantlcawley@gmail.com>
2021-06-02 20:03:08 -0400

    Updated project cards to adjust to screen size

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, ButtonGroup, Container } from 'react-bootstrap';
+
+import "./style.css"
 
 const card = (props) => {
     return (
-        <>
-        <Card style={{ width: '18rem' }}>
+        <Container fluid>
+            <Card className="card" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={props.asset} />
                 <Card.Body>
                     <Card.Title>{props.title}</Card.Title>
                     <Card.Text>{props.text}</Card.Text>
-                    <Button size="sm" href={props.appLink} variant="danger">Application</Button>
-                    <Button size="sm" href={props.repoLink} variant="danger">Repository</Button>
+                    <ButtonGroup>
+                        <Button size="sm" href={props.appLink} variant="danger">Application</Button>
+                        <Button size="sm" href={props.repoLink} variant="danger">Repository</Button>
+                    </ButtonGroup>
                 </Card.Body>
-        </Card>        
-        </>
+            </Card>
+        </Container>
 
     );
 }
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,5 +1,6 @@
 // import React, { useEffect, useState } from 'react'
 // import { useAppContext } from '../../utils/AppContext'
+import { Col, Container, Row } from 'react-bootstrap';
 import PageTitle from '../../components/PageTitle'
 import ProjectCard from '../../components/ProjectCard'
 
@@ -13,18 +14,24 @@ import "./style.css"
 
 const PortfolioPage = () => {
     return (
-        <div className="container mt-sm-5 mt-4">
-            <PageTitle title="Portfolio" subtitle="A sample of the coding projects and assignments I've completed, with links to deployed projects and GitHub repositories"></PageTitle>
+        <Container>
+            <PageTitle title="Portfolio" subtitle="Previews of completed coding projects and assignments, with links to deployed projects and GitHub repositories"></PageTitle>
+            <Row xs={1} md={2} lg={3}>
+                <Col>
+                    <ProjectCard asset={Burger} title="Burger Eater" text="A simple application that allows the user to submit names of burgers, then 'eat' them by clicking a button that populates next to it's name." appLink="https://floating-ocean-83726.herokuapp.com/" repoLink="https://github.com/cawleygl/burger"></ProjectCard>
+                </Col>
+                <Col>
+                    <ProjectCard asset={Recipe} title="Nutritious Recipe Generator" text="A web application that allows users to quickly display recipes from a recipe API in different ways, to inspire creativity in the kitchen." appLink="https://egivilza.github.io/NutritiousRecipeGenerator/" repoLink="https://github.com/EgiVilza/NutritiousRecipeGenerator"></ProjectCard>
+                </Col>
+                <Col>
+                    <ProjectCard asset={Weather} title="Weather Dashboard" text="A weather dashboard application that shows current weather conditions in any city, as well as a 5-day forecast." appLink="https://cawleygl.github.io/weather-dashboard/" repoLink="https://github.com/cawleygl/weather-dashboard"></ProjectCard>
 
-            <ProjectCard asset={Burger} title="Burger Eater" text="A simple application that allows the user to submit names of burgers, then 'eat' them by clicking a button that populates next to it's name." appLink="https://floating-ocean-83726.herokuapp.com/" repoLink="https://github.com/cawleygl/burger"></ProjectCard>
-
-            <ProjectCard asset={Recipe} title="Nutritious Recipe Generator" text="A web application that allows users to quickly display recipes from a recipe API in different ways, to inspire creativity in the kitchen." appLink="https://egivilza.github.io/NutritiousRecipeGenerator/" repoLink="https://github.com/EgiVilza/NutritiousRecipeGenerator"></ProjectCard>
-
-            <ProjectCard asset={Weather} title="Weather Dashboard" text="A weather dashboard application that shows current weather conditions in any city, as well as a 5-day forecast" appLink="https://cawleygl.github.io/weather-dashboard/" repoLink="https://github.com/cawleygl/weather-dashboard"></ProjectCard>
-
-            <ProjectCard asset={Planner} title="Work Day Scheduler" text="An application that allows the user to schedule out events in a work day, save them for later reference, and see which events have passed and which are still to come." appLink="https://cawleygl.github.io/workday-planner/" repoLink="https://github.com/cawleygl/workday-planner"></ProjectCard>
-
-        </div>
+                </Col>
+                <Col>
+                    <ProjectCard asset={Planner} title="Work Day Scheduler" text="An application that allows the user to schedule out events in a work day, save them for later reference, and see which events have passed and which are still to come." appLink="https://cawleygl.github.io/workday-planner/" repoLink="https://github.com/cawleygl/workday-planner"></ProjectCard>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
